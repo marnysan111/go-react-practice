9d216a65159d58ffc4f4b691f95fdfd2027ff83e
marnysan111 <o-masato@outlook.jp>
2021-07-01 15:21:47 +0900

    開発環境は出来てる

---
+++ go/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/api", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "test"})
+	})
+
+	r.Run(":8080")
+}
--- react/goreact/src/App.js
+++ react/goreact/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
 
 function App() {
+  const [post, setPosts] = useState([])
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res => {
+        setPosts(res.data)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+        <h1>Todo</h1>
+        {post.map((obj) => <li>{obj.id}：{obj.title}</li>)}
+    </React.Fragment>
   );
 }
 
